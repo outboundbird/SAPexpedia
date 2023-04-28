23dbe722907eb9c3b94e45cdb3043c8c30e99b86
Siying H <siying.huang-ext@sanofi.com>
2023-04-28 18:27:31 +0200

    must have the space!

--- R/new_word.R
+++ R/new_word.R
@@ -1,14 +1,17 @@
-# function create a word doc here
+#' create Rmd file to generate Word document.
+#' create yaml header for rmd to output word document
+#' @return rmd document
+#' @export
 create_word <- function() {
 
   header <-
   glue::glue('---
 title: "$"
 author: " "
-date:"{Sys.Date()}"
+date: "{Sys.Date()}"
 output:
   word_document:
     reference_docx: "eSAP_Template_v2.1_BWE.docx"
 ---')
-  rstudioapi::documentNew(header, type = "rmd")
+  rstudioapi::documentNew(header, type = "rmarkdown")
 }
